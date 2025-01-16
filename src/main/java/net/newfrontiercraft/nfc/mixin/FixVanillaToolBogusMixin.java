d9dfe6906aee58d78568d4a9ab95c454a33a5ede
Atilist <71230342+Atilist@users.noreply.github.com>
2025-01-16 20:11:35 +0100

    Mixin to increase Tool Requirement for Iron Ore
    This has been done to unfix the Unfix done by Station API.

---
+++ src/main/java/net/newfrontiercraft/nfc/mixin/FixVanillaToolBogusMixin.java
@@ -0,0 +1,25 @@
+package net.newfrontiercraft.nfc.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ToolItem;
+import net.minecraft.item.ToolMaterial;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PickaxeItem.class)
+public abstract class FixVanillaToolBogusMixin extends ToolItem {
+
+    public FixVanillaToolBogusMixin(int id, int damageBoost, ToolMaterial toolMaterial, Block[] effectiveOn) {
+        super(id, damageBoost, toolMaterial, effectiveOn);
+    }
+
+    @Inject(at = @At("HEAD"), method = "isSuitableFor", remap = false, cancellable = true)
+    private void suppressOres(Block block, CallbackInfoReturnable<Boolean> cir) {
+        if (block == Block.IRON_ORE) {
+            cir.setReturnValue(toolMaterial.getMiningLevel() >= 3);
+        }
+    }
+}
