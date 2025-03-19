73c6bf23ee64a5d6f132669fc99636cdc73657f4
Atilist <71230342+Atilist@users.noreply.github.com>
2025-03-19 20:38:53 +0100

    Tab Alignment Corrections

--- src/main/java/net/newfrontiercraft/nfc/compat/ami/carpentry/CarpentryRecipeCategory.java
+++ src/main/java/net/newfrontiercraft/nfc/compat/ami/carpentry/CarpentryRecipeCategory.java
@@ -12,7 +12,7 @@ import org.jetbrains.annotations.NotNull;
 public class CarpentryRecipeCategory implements RecipeCategory {
 
     @NotNull
-    private final AMIDrawable background = DrawableHelper.createDrawable("/assets/nfc/stationapi/gui/carpentry.png", 8, 8, 160, 100);
+    private final AMIDrawable background = DrawableHelper.createDrawable("/assets/nfc/stationapi/gui/carpentry.png", 12, 4, 160, 70);
 
     @NotNull
     @Override
@@ -45,11 +45,11 @@ public class CarpentryRecipeCategory implements RecipeCategory {
     public void setRecipe(@NotNull RecipeLayout recipeLayout, @NotNull RecipeWrapper recipeWrapper) {
         GuiItemStackGroup guiItemStacks = recipeLayout.getItemStacks();
         int xOffset = 29;
-        int yOffset = 8;
-        guiItemStacks.init(0, false, 20 + xOffset, yOffset + 20);
+        int yOffset = 12;
+        guiItemStacks.init(0, false, xOffset - 6, yOffset + 18);
         guiItemStacks.setFromRecipe(0, recipeWrapper.getInputs().get(0));
         for (int i = 0; i < recipeWrapper.getOutputs().size(); i++) {
-            guiItemStacks.init(i + 1, true, xOffset + (i % 4) * 18 + 54, yOffset + (i / 4) * 18);
+            guiItemStacks.init(i + 1, true, xOffset + (i % 4) * 18 + 56, yOffset + (i / 4) * 18);
         }
         for (int i = 0; i < recipeWrapper.getOutputs().size(); i++) {
             guiItemStacks.setFromRecipe(i + 1, recipeWrapper.getOutputs().get(i));
