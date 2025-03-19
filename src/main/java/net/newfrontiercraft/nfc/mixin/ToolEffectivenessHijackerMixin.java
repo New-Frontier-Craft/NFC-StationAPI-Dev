9f144fd6a53280f2ea66dde6904184c5ea706344
Atilist <71230342+Atilist@users.noreply.github.com>
2025-03-19 17:26:23 +0100

    Hijack Tool Effectiveness

---
+++ src/main/java/net/newfrontiercraft/nfc/mixin/ToolEffectivenessHijackerMixin.java
@@ -0,0 +1,17 @@
+package net.newfrontiercraft.nfc.mixin;
+
+import net.minecraft.item.ItemStack;
+import net.modificationstation.stationapi.api.block.BlockState;
+import net.modificationstation.stationapi.impl.item.ToolEffectivenessImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ToolEffectivenessImpl.class)
+public class ToolEffectivenessHijackerMixin {
+    @Inject(at = @At("HEAD"), method = "shouldApplyCustomLogic", remap = false, cancellable = true)
+    private static void hijackMethod(ItemStack item, BlockState state, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+}
