cba50beeda291ff4735744838e795820772bcfe5
Atilist <71230342+Atilist@users.noreply.github.com>
2025-04-19 19:33:50 +0200

    Slab Torch Updating Logic

--- src/main/java/net/newfrontiercraft/nfc/mixin/TorchPlacementMixin.java
+++ src/main/java/net/newfrontiercraft/nfc/mixin/TorchPlacementMixin.java
@@ -7,6 +7,7 @@ import net.newfrontiercraft.nfc.block.LazySlabTemplate;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TorchBlock.class)
@@ -25,6 +26,45 @@ public class TorchPlacementMixin {
             int rotation = world.getBlockState(x, y - 1, z).get(LazySlabTemplate.ROTATIONS);
             if (rotation < 2) {
                 cir.setReturnValue(true);
+            }
+        }
+    }
+
+    @Inject(at = @At("HEAD"), method = "onPlaced(Lnet/minecraft/world/World;III)V", cancellable = true)
+    private void rotateTorchCorrectly(World world, int x, int y, int z, CallbackInfo ci) {
+        int belowId = world.getBlockId(x, y - 1, z);
+        if (belowId == Block.SLAB.id) {
+            world.setBlockMeta(x, y, z, 5);
+            ci.cancel();
+        } else if (belowId != 0) {
+            Block belowBlock = Block.BLOCKS[belowId];
+            if (!(belowBlock instanceof LazySlabTemplate)) {
+                return;
+            }
+            int rotation = world.getBlockState(x, y - 1, z).get(LazySlabTemplate.ROTATIONS);
+            if (rotation < 2) {
+                world.setBlockMeta(x, y, z, 5);
+                ci.cancel();
+            }
+        }
+    }
+
+    @Inject(at = @At("HEAD"), method = "neighborUpdate", cancellable = true)
+    private void rotateTorchCorrectly(World world, int x, int y, int z, int id, CallbackInfo ci) {
+        if (world.getBlockMeta(x, y, z) != 5) {
+            return;
+        }
+        int belowId = world.getBlockId(x, y - 1, z);
+        if (belowId == Block.SLAB.id) {
+            ci.cancel();
+        } else if (belowId != 0) {
+            Block belowBlock = Block.BLOCKS[belowId];
+            if (!(belowBlock instanceof LazySlabTemplate)) {
+                return;
+            }
+            int rotation = world.getBlockState(x, y - 1, z).get(LazySlabTemplate.ROTATIONS);
+            if (rotation < 2) {
+                ci.cancel();
             }
         }
     }
