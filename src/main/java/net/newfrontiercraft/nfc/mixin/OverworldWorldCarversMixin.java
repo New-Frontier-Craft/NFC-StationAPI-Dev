c78054a817544790f401e87a85c6a969c0927b56
Ralf2oo2 <ralf2oo2@hotmail.com>
2025-04-27 18:42:25 +0200

    Remove testing code

--- src/main/java/net/newfrontiercraft/nfc/mixin/OverworldWorldCarversMixin.java
+++ src/main/java/net/newfrontiercraft/nfc/mixin/OverworldWorldCarversMixin.java
@@ -3,11 +3,11 @@ package net.newfrontiercraft.nfc.mixin;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.gen.Generator;
 import net.minecraft.world.gen.chunk.OverworldChunkGenerator;
 import net.newfrontiercraft.nfc.world.gen.carver.RavineWorldCarver;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -15,12 +15,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(OverworldChunkGenerator.class)
 public class OverworldWorldCarversMixin {
     @Shadow private World world;
-    @Shadow private Generator cave;
+    @Unique
     private RavineWorldCarver ravineWorldCarver = new RavineWorldCarver();
+    
     @Inject(method = "getChunk", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/Generator;place(Lnet/minecraft/world/chunk/ChunkSource;Lnet/minecraft/world/World;II[B)V"))
     void nfcRunCarvers(int chunkX, int chunkZ, CallbackInfoReturnable<Chunk> cir, @Local byte[] blocks){
         OverworldChunkGenerator overworldChunkGenerator = OverworldChunkGenerator.class.cast(this);
-        //cave.place(overworldChunkGenerator, world, chunkX, chunkZ, blocks);
         ravineWorldCarver.place(overworldChunkGenerator, world, chunkX, chunkZ, blocks);
     }
 }
