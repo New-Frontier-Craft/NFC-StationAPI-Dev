7b128139b003c7dcf9f4d3794a15ceba2e22cdb0
Atilist <71230342+Atilist@users.noreply.github.com>
2025-05-11 20:41:16 +0200

    Dead Tree Code Cleanup and better Location Check

--- src/main/java/net/newfrontiercraft/nfc/world/gen/feature/DeadTreeFeature.java
+++ src/main/java/net/newfrontiercraft/nfc/world/gen/feature/DeadTreeFeature.java
@@ -12,45 +12,19 @@ public class DeadTreeFeature extends Feature {
     public boolean generate(World world, Random random, int x, int y, int z) {
         int l = random.nextInt(3) + 5;
         if(y >= 1 && y + l + 1 <= 128) {
-            boolean flag = true;
-
-            int x0;
-            int j1;
-            int maxFallenLeaves;
-            for(x0 = y; x0 <= y + 1 + l; ++x0) {
-                byte z0 = 1;
-                if(x0 == y) {
-                    z0 = 0;
-                }
-
-                if(x0 >= y + 1 + l - 2) {
-                    z0 = 2;
-                }
-
-                for(j1 = x - z0; j1 <= x + z0 && flag; ++j1) {
-                    for(maxFallenLeaves = z - z0; maxFallenLeaves <= z + z0 && flag; ++maxFallenLeaves) {
-                        if(x0 >= 0 && x0 < 128) {
-                            int falling = world.getBlockId(j1, x0, maxFallenLeaves);
-                            if(falling != 0) {
-                                flag = false;
-                            }
-                        } else {
-                            flag = false;
-                        }
-                    }
-                }
-            }
-
-            x0 = x;
+            int x0 = x;
             int i22 = z;
-            j1 = world.getBlockId(x, y - 1, z);
-            if((j1 == Block.GRASS.id || j1 == Block.DIRT.id || j1 == Block.SOUL_SAND.id) && y < 128 - l - 1) {
-                maxFallenLeaves = random.nextInt(3) > 0 ? random.nextInt(24) + 8 : 0;
+            int bottomBlock = world.getBlockId(x, y - 1, z);
+            if (world.getBlockId(x, y + 1, z) != 0) {
+                return false;
+            }
+            if((bottomBlock == Block.GRASS.id || bottomBlock == Block.DIRT.id || bottomBlock == Block.SOUL_SAND.id) && y < 128 - l - 1) {
+                int maxFallenLeaves = random.nextInt(3) > 0 ? random.nextInt(24) + 8 : 0;
                 boolean z23 = false;
                 int swayHeight = -1;
                 if(random.nextBoolean()) {
                     swayHeight = l - (random.nextInt(3) + 3);
-                    switch(random.nextInt(3)) {
+                    switch(random.nextInt(4)) {
                         case 1:
                             x0 = x - 1;
                             break;
