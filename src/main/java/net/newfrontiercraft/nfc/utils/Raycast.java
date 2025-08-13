621c2317fde8d669d87e4a9cc2575dbc2a494068
Atilist <71230342+Atilist@users.noreply.github.com>
2025-08-13 13:09:39 +0200

    Simplify Raycast

--- src/main/java/net/newfrontiercraft/nfc/utils/Raycast.java
+++ src/main/java/net/newfrontiercraft/nfc/utils/Raycast.java
@@ -6,16 +6,7 @@ import net.minecraft.util.math.Vec3d;
 
 public class Raycast {
     public static HitResult raycast(LivingEntity origin, double distance, Vec3d lookVector, Vec3d previousPlayerLocation) {
-        Vec3d position = getPosition(previousPlayerLocation);
-
-        Vec3d target = position.add(lookVector.x * distance, lookVector.y * distance, lookVector.z * distance);
-        return origin.world.raycast(position, target);
-    }
-
-    public static Vec3d getPosition(Vec3d previousPlayerLocation) {
-        double var2 = previousPlayerLocation.x;
-        double var4 = previousPlayerLocation.y;
-        double var6 = previousPlayerLocation.z;
-        return Vec3d.createCached(var2, var4, var6);
+        Vec3d target = previousPlayerLocation.add(lookVector.x * distance, lookVector.y * distance, lookVector.z * distance);
+        return origin.world.raycast(previousPlayerLocation, target);
     }
 }
