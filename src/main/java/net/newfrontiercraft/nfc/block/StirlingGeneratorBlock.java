c8a487ed8ae99561f8da642627d8df6c5c8e8ca1
Atilist <71230342+Atilist@users.noreply.github.com>
2025-08-21 20:01:38 +0200

    Particles for Stirling Generator

--- src/main/java/net/newfrontiercraft/nfc/block/StirlingGeneratorBlock.java
+++ src/main/java/net/newfrontiercraft/nfc/block/StirlingGeneratorBlock.java
@@ -8,6 +8,8 @@ import net.modificationstation.stationapi.api.util.Identifier;
 import net.newfrontiercraft.nfc.api.TorqueGenerator;
 import net.newfrontiercraft.nfc.block.entity.HeatCoilBlockEntity;
 
+import java.util.Random;
+
 public class StirlingGeneratorBlock extends LazyBlockTemplate implements TorqueGenerator {
     public StirlingGeneratorBlock(Identifier identifier, Material material, float hardness, BlockSoundGroup blockSounds) {
         super(identifier, material, hardness, blockSounds);
@@ -15,11 +17,20 @@ public class StirlingGeneratorBlock extends LazyBlockTemplate implements TorqueG
 
     @Override
     public int getTorque(World world, int x, int y, int z) {
-        return getTorqueFromHeatCoil(world, x + 1, y, z)
+        int additionalTorque = getTorqueFromHeatCoil(world, x + 1, y, z)
                 + getTorqueFromHeatCoil(world, x - 1, y, z)
                 + getTorqueFromHeatCoil(world, x, y, z + 1)
-                + getTorqueFromHeatCoil(world, x, y, z - 1)
-                + getTorqueFromOtherBlock(world, x, y - 1, z);
+                + getTorqueFromHeatCoil(world, x, y, z - 1);
+        if (additionalTorque < 1) {
+            world.setBlockMeta(x, y, z, 0);
+        } else if (additionalTorque < 4) {
+            world.setBlockMeta(x, y, z, 1);
+        } else if (additionalTorque < 7) {
+            world.setBlockMeta(x, y, z, 2);
+        } else {
+            world.setBlockMeta(x, y, z, 3);
+        }
+        return additionalTorque + getTorqueFromOtherBlock(world, x, y - 1, z);
     }
 
     private int getTorqueFromHeatCoil(World world, int x, int y, int z) {
@@ -40,5 +51,16 @@ public class StirlingGeneratorBlock extends LazyBlockTemplate implements TorqueG
             }
         }
         return 0;
+    }
+
+    @Override
+    public void randomDisplayTick(World world, int x, int y, int z, Random random) {
+        int particleCount = world.getBlockMeta(x, y, z) * 4;
+        for (int i = 0; i < particleCount; i++) {
+            float xOffset = random.nextFloat();
+            float yOffset = random.nextFloat();
+            float zOffset = random.nextFloat();
+            world.addParticle("smoke", x + xOffset, y + yOffset, z + zOffset, 0.0D, 0.0D, 0.0D);
+        }
     }
 }
