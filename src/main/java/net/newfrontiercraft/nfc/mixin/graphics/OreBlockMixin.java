62ee7e4308cea3b1975715ffeda17b6e4ec2a40e
Atilist <71230342+Atilist@users.noreply.github.com>
2025-09-05 18:04:25 +0200

    Vanilla Ore Tooltips

---
+++ src/main/java/net/newfrontiercraft/nfc/mixin/graphics/OreBlockMixin.java
@@ -0,0 +1,31 @@
+package net.newfrontiercraft.nfc.mixin.graphics;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.OreBlock;
+import net.minecraft.client.resource.language.TranslationStorage;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.modificationstation.stationapi.api.client.item.CustomTooltipProvider;
+import net.newfrontiercraft.nfc.utils.ToolTierEnum;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(OreBlock.class)
+public class OreBlockMixin implements CustomTooltipProvider {
+    @Override
+    public @NotNull String[] getTooltip(ItemStack stack, String originalTooltip) {
+        ToolTierEnum toolTierEnum;
+        Item item = stack.getItem();
+        if (item == Block.GOLD_ORE.asItem()) {
+            toolTierEnum = ToolTierEnum.WOOD;
+        } else if (item == Block.IRON_ORE.asItem()) {
+            toolTierEnum = ToolTierEnum.IRON;
+        } else if (item == Block.DIAMOND_ORE.asItem()) {
+            toolTierEnum = ToolTierEnum.DIAMOND;
+        } else {
+            return new String[]{originalTooltip};
+        }
+        return new String[]{toolTierEnum.getColourCode() + originalTooltip,
+                "§7" + TranslationStorage.getInstance().get("tool_tier.tier") + ": " + toolTierEnum.getColourCode() + TranslationStorage.getInstance().get(toolTierEnum.getName())};
+    }
+}
