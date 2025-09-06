c094ea0024c9b65f7f0992d3184745a2f8a9e0de
Atilist <71230342+Atilist@users.noreply.github.com>
2025-09-06 13:14:41 +0200

    Change Default Value for Music Interval

--- src/main/java/net/newfrontiercraft/nfc/config/AudioConfig.java
+++ src/main/java/net/newfrontiercraft/nfc/config/AudioConfig.java
@@ -4,7 +4,7 @@ import net.glasslauncher.mods.gcapi3.api.ConfigEntry;
 
 public class AudioConfig {
     @ConfigEntry(name = "Music Interval", description = "Determines how the long the pause is between songs. 0 means a constant stream of music, 12000 is the default.")
-    public Integer musicInterval = 0;
+    public Integer musicInterval = 12000;
 
     @ConfigEntry(name = "Randomize Music Pitch", description = "Causes the music pitch to be chosen randomly, the values randing from 0.5 to 1.5")
     public Boolean randomizeMusicPitch = false;
