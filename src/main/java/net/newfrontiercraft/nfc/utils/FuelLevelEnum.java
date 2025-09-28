ad5ae75da61816619a2be79c751d4a4352f89f7f
Atilist <71230342+Atilist@users.noreply.github.com>
2025-09-28 17:13:43 +0200

    Adjust Fuel Level Values

--- src/main/java/net/newfrontiercraft/nfc/utils/FuelLevelEnum.java
+++ src/main/java/net/newfrontiercraft/nfc/utils/FuelLevelEnum.java
@@ -1,12 +1,12 @@
 package net.newfrontiercraft.nfc.utils;
 
 public enum FuelLevelEnum {
-    COLD(500),
-    WARM(1500),
-    HOT(2500),
-    SEARING(3500),
-    BLAZING(4500),
-    SUPERHEATED(5500);
+    COLD(250),
+    WARM(450),
+    HOT(650),
+    SEARING(850),
+    BLAZING(1050),
+    SUPERHEATED(1250);
 
     private final int heat;
 
