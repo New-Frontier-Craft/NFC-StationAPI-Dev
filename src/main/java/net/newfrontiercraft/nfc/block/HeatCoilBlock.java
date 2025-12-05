ae482d3e490289cda93dd66da48f39ce6c314b21
Atilist <71230342+Atilist@users.noreply.github.com>
2025-12-05 20:08:32 +0100

    Heat Coil Documentation

--- src/main/java/net/newfrontiercraft/nfc/block/HeatCoilBlock.java
+++ src/main/java/net/newfrontiercraft/nfc/block/HeatCoilBlock.java
@@ -14,6 +14,7 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.modificationstation.stationapi.api.block.BlockState;
+import net.modificationstation.stationapi.api.block.HasCustomBlockItemFactory;
 import net.modificationstation.stationapi.api.registry.ItemRegistry;
 import net.modificationstation.stationapi.api.state.StateManager;
 import net.modificationstation.stationapi.api.state.property.IntProperty;
@@ -24,10 +25,12 @@ import net.modificationstation.stationapi.api.util.Namespace;
 import net.modificationstation.stationapi.api.util.math.Direction;
 import net.modificationstation.stationapi.api.world.BlockStateView;
 import net.newfrontiercraft.nfc.block.entity.HeatCoilBlockEntity;
+import net.newfrontiercraft.nfc.block.item.HeatSiphonBlockItem;
 import net.newfrontiercraft.nfc.events.init.ItemListener;
 import net.newfrontiercraft.nfc.utils.BoxUtil;
 import net.newfrontiercraft.nfc.utils.CoilDamageCooldown;
 
+@HasCustomBlockItemFactory(HeatSiphonBlockItem.class)
 public class HeatCoilBlock extends TemplateBlockWithEntity {
 
     public static final IntProperty HEAT = IntProperty.of("heat", 0, 3);
